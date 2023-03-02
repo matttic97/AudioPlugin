fb451dc94992c2180edb3c84daa42028e0473f86
Matic Isovski <maticisovski@hotmail.com>
2023-03-02 19:06:13 +0100

    added basic slider visuals

--- PluginEditor.cpp
+++ PluginEditor.cpp
@@ -2,6 +2,70 @@
 #include "PluginEditor.h"
 
 
+void LookAndFeel::drawRotarySlider(juce::Graphics &g, int x, int y, int width, int height, float sliderPosProportional,
+                                   float rotaryStartAngle, float rotaryEndAngle, juce::Slider &)
+{
+    using namespace juce;
+
+    auto bounds = Rectangle<float>(x, y, width, height);
+
+    g.setColour(Colour(0, 102, 204));
+    g.fillEllipse(bounds);
+
+    g.setColour(Colour(0, 76, 153));
+    g.drawEllipse(bounds, 2.f);
+
+    auto center = bounds.getCentre();
+
+    Path p;
+
+    Rectangle<float> r;
+    r.setLeft(center.getX() - 2);
+    r.setRight(center.getX() + 2);
+    r.setTop(bounds.getY());
+    r.setBottom(center.getY());
+
+    p.addRectangle(r);
+
+    jassert(rotaryStartAngle < rotaryEndAngle);
+
+    auto sliderAngRad = jmap(sliderPosProportional, 0.f, 1.f, rotaryStartAngle, rotaryEndAngle);
+
+    p.applyTransform(AffineTransform().rotated(sliderAngRad, center.getX(), center.getY()));
+
+    g.fillPath(p);
+}
+
+//==============================================================================
+void RotarySliderWithLabels::paint(juce::Graphics &g)
+{
+    using namespace juce;
+
+    auto startAng = degreesToRadians(180.f + 45.f);
+    auto endAng = degreesToRadians(180.f - 45.f) + MathConstants<float>::twoPi;
+
+    auto range = getRange();
+
+    auto sliderBounds = getSliderBounds();
+
+    getLookAndFeel().drawRotarySlider(
+            g,
+            sliderBounds.getX(),
+            sliderBounds.getY(),
+            sliderBounds.getWidth(),
+            sliderBounds.getHeight(),
+            jmap(getValue(), range.getStart(), range.getEnd(), 0.0, 1.0),
+            startAng,
+            endAng,
+            *this);
+}
+
+juce::Rectangle<int> RotarySliderWithLabels::getSliderBounds() const
+{
+    return getLocalBounds();
+}
+
+//==============================================================================
 ResponseCurveComponent::ResponseCurveComponent(AudioPluginAudioProcessor& p) : processorRef(p)
 {
     const auto& params = processorRef.getParameters();
@@ -116,6 +180,7 @@ void ResponseCurveComponent::paint (juce::Graphics& g)
     g.setColour(Colours::white);
     g.strokePath(responseCurve, PathStrokeType(2.f));
 }
+
 //==============================================================================
 AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAudioProcessor& p)
     : AudioProcessorEditor (&p), processorRef (p),
