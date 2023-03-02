9e0fc3efa5a08a205ddaa15903eae1dc1ca1bb84
Matic Isovski <maticisovski@hotmail.com>
2023-03-02 21:40:33 +0100

    change cut filter ranges

--- PluginProcessor.cpp
+++ PluginProcessor.cpp
@@ -273,13 +273,13 @@ juce::AudioProcessorValueTreeState::ParameterLayout AudioPluginAudioProcessor::c
     layout.add(std::make_unique<juce::AudioParameterFloat>(
             "LowCut Freq",
             "LowCut Freq",
-            juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+            juce::NormalisableRange<float>(20.f, 1000.f, 1.f, 0.25f),
             20.f));
 
     layout.add(std::make_unique<juce::AudioParameterFloat>(
             "HighCut Freq",
             "HighCut Freq",
-            juce::NormalisableRange<float>(20.f, 20000.f, 1.f, 0.25f),
+            juce::NormalisableRange<float>(1000.f, 20000.f, 1.f, 0.25f),
             20000.f));
 
     layout.add(std::make_unique<juce::AudioParameterFloat>(
